84f4b751b121cf12beeb8c7873b201716c99139b
Sssvoya <sobolskayak@mail.ru>
2022-04-29 17:26:50 +0400

    1

---
+++ src/test/java/ru/guwfa/auction/AuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.guwfa.auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
