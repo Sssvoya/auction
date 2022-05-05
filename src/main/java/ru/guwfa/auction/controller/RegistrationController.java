3d723ae0ef91acc1925800d0b64dc796443be1d4
AnastasiyaLa <nastyalachina@mail.ru>
2022-05-05 20:06:00 +0400

    3.4

--- src/main/java/ru/guwfa/auction/controller/RegistrationController.java
+++ src/main/java/ru/guwfa/auction/controller/RegistrationController.java
@@ -1,8 +1,28 @@
 package ru.guwfa.auction.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RegistrationController {
     private final static String CAPTCHA_URL = "https://www.google.com/recaptcha/api/siteverify?secret=%s&response=%s";
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(){
+
+    }
+
+    @GetMapping("/activate/{code}")
+    public String activate(){
+
+    }
 }
+
+
+
