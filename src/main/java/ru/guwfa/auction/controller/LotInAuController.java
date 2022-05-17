59fe4643e065014ab975529f213df4cec6beb2f3
Sssvoya <sobolskayak@mail.ru>
2022-05-17 22:33:11 +0400

    5

--- src/main/java/ru/guwfa/auction/controller/LotInAuController.java
+++ src/main/java/ru/guwfa/auction/controller/LotInAuController.java
@@ -5,4 +5,5 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class LotInAuController {
     private final static int PAGE_SIZE = 6;
+
 }
--- src/main/java/ru/guwfa/auction/controller/commentaryController.java
+++ src/main/java/ru/guwfa/auction/controller/commentaryController.java
@@ -5,4 +5,5 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class commentaryController {
     private final static int PAGE_SIZE = 5;
+
 }
